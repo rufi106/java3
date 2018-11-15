dcd4b1d960b1c8ed2ae4b44def00b6c87e7ddc20
unknown <rafal1006@gmail.com>
2018-11-15 13:39:08 +0100

    zadania v1.01

--- Main.java
+++ Main.java
@@ -1,29 +1,21 @@
-package pl.edu.wszib;
+﻿package pl.edu.wszib;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/*
-Kod w PHP:
-<?php if(trim($_GET["firstname"])&&trim($_GET["lastname"]))
-	file_put_contents("list", $_GET["firstname"].",".$_GET["lastname"]."\n", FILE_APPEND) or die("nie można zapisać do pliku");
-$people=file('list', FILE_IGNORE_NEW_LINES) or die("nie można otworzyć pliku");
-foreach($people as $person)
-	echo (explode(",", $person)[0].".".explode(",", $person)[1].$occurrenceNum[explode(",", $person)[0].",".explode(",", $person)[1]]++."@company.com<br>\n"); ?>
-5 linijek z zapisem do pliku i sprawdzaniem poprawności
- */
+
 
 public class Main
 {
     public static void main(String[] args)
     {
-        List<Person> list = new ArrayList<Person>(); //PHP jest lepsze
+        List<Person> list = new ArrayList<Person>(); 
         String firstName;
         String lastName;
         String mode;
         int lastID;
-        Scanner input = new Scanner(System.in); //PHP jest lepsze
+        Scanner input = new Scanner(System.in); 
         while(true)
         {
             System.out.println("p - wypisz | a - dodaj | q - wyjdź");
@@ -32,7 +24,7 @@ public class Main
                 System.exit(0);
             if(mode.equals("p"))
             {
-                for (Person person : list) { //PHP jest lepsze
+                for (Person person : list) { 
                     System.out.println(person.outputEmail());
                 }
             }
@@ -44,8 +36,8 @@ public class Main
                 System.out.print("Nazwisko: ");
                 lastName = input.next();
 
-                for (Person person : list) //PHP jest lepsze
-                    if (person.firstDotLast.equals(firstName + '.' + lastName)) //PHP jest o wiele lepsze
+                for (Person person : list) 
+                    if (person.firstDotLast.equals(firstName + '.' + lastName)) 
                         lastID++;
 
                 Person newPerson = new Person(firstName, lastName, lastID);
